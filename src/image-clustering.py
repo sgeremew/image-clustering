a3193febe6ae589617acd44850ab1288a6c10298
Samuel Geremew <samuelgeremew@Samuels-MacBook-Pro.local>
2019-04-10 09:26:03 -0400

    added function to import data from a file

--- src/image-clustering.py
+++ src/image-clustering.py
@@ -1,30 +1,52 @@
+import pandas as pd
+import numpy as np
+import random
+
 # Image Clustering
 # Samuel Geremew
 
 # Using K-Means algorithm this program will take in a dataset of images of hand-
 # written digits and classify them.
 
+# import_data():	Takes in a file and returns a DataFrame of records
+# Parameters:	file: file of data points/records
+# Returns:	data: dataset of points (DataFrame)
+def import_data(file):
+	# Since features do not have labels we set header=None so that instead we store 
+	# the index as the attribute names
+	data = pd.read_csv(file,header=None)
+	return data
 
-# seed: random number seed (int)
-# Parameters:	data: dataset of points (DataFrame)
-# 				seed: for our random number generator
-# Returns:	centroids: array of centroids (numpy.ndarray)
-def find_centroids(data, seed=3):
+# # seed: random number seed (int)
+# # Parameters:	data: dataset of points (DataFrame)
+# # 				seed: for our random number generator
+# # Returns:	centroids: array of centroids (numpy.ndarray)
+# def find_centroids(data, seed=3):
 
 
-# pairwise_minimums():	finds the minimum distance of a point to a set of points
-# Parameters:	data: dataset of points (DataFrame)
-# 				centroids: array of centroids (numpy.ndarray)
-# Returns:	minimum: the centroid that was closest to data[i,:] (numpy.ndarray)
-def pairwise_minimums(data, centroids):
+# # pairwise_minimums():	finds the minimum distance of a point to a set of points
+# # Parameters:	data: dataset of points (DataFrame)
+# # 				centroids: array of centroids (numpy.ndarray)
+# # Returns:	minimum: the centroid that was closest to data[i,:] (numpy.ndarray)
+# def pairwise_minimums(data, centroids):
 
 
-# clusters():	uses the K-Means clustering algorithm to identify clusters in 
-#				the dataset
-# Parameters:	data: dataset of points (DataFrame)
-# 				k: number of clusters (int)
-# Returns:	array of labels (numpy.ndarray) and an array of centroids 
-# 			(numpy.ndarray)
-def clusters(data, k):
+# # clusters():	uses the K-Means clustering algorithm to identify clusters in 
+# #				the dataset
+# # Parameters:	data: dataset of points (DataFrame)
+# # 				k: number of clusters (int)
+# # Returns:	array of labels (numpy.ndarray) and an array of centroids 
+# # 			(numpy.ndarray)
+# def clusters(data, k):
+
+
+
+def main():
+	file = "../iris-test.dat"
+	data = import_data(file)
+	
+	# print(data)
+
+main()
 
 
