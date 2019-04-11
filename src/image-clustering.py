0ace5faea55dd56a81ddc67695bc0775423db9d6
Samuel Geremew <samuelgeremew@Samuels-MacBook-Pro.local>
2019-04-11 08:03:08 -0400

    working on pairwise_minimums func

--- src/image-clustering.py
+++ src/image-clustering.py
@@ -12,15 +12,15 @@ K = 3	# used for the iris-test.dat data set
 # Image Clustering
 # Samuel Geremew
 
-# Using K-Means algorithm this program will take in a data set of images of hand-
-# written digits and classify them.
+# Using K-Means algorithm this program will take in a data set of images of 
+# hand-written digits and classify them.
 
 # import_data():	Takes in a file and returns a DataFrame of records
 # Parameters:	file: file of data points/records
 # Returns:	data: data set of points (DataFrame)
 def import_data(file):
-	# Since features do not have labels we set header=None so that instead we store 
-	# the index as the attribute names
+	# Since features do not have labels we set header=None and sep=' ' so that 
+	# instead we store the index as the attribute names
 	data = pd.read_csv(file,sep=' ',header=None)
 	return data
 
@@ -30,6 +30,7 @@ def import_data(file):
 def find_centroids(data):
 	data = shuffle(data)
 	centroids = data.iloc[:K]
+	print(type(centroids))
 	return centroids
 
 
@@ -49,12 +50,22 @@ def euclidean_dist(p,q):
 # pairwise_minimums():	finds the minimum distance of a point to a set of points
 # Parameters:	data: data set of points (DataFrame)
 # 				centroids: array of centroids (DataFrame)
-# Returns:	minimum: the centroid that was closest to data[i,:] (numpy.ndarray)
+# Returns:	labels: the centroids that were closest to data points
 def pairwise_minimums(data, centroids):
 
-	# point = data.iloc[0]
-	# min_dist = centroids.iloc[0] - point
-	return None
+	labels = []
+
+	for i in range(len(data)):
+		min_dist = euclidean_dist(data.iloc[i],centroids.iloc[0])
+		for c in centroids:
+			dist = euclidean_dist(data.iloc[i],c)
+			if(dist < min_dist):
+				min_dist = dist
+				label = c
+		labels.append(label)
+
+
+	return labels
 
 # clusters():	uses the K-Means clustering algorithm to identify clusters in 
 #				the data set
@@ -69,11 +80,12 @@ def clusters(data, k):
 def main():
 	file = "../iris-test.dat"
 	data = import_data(file)
+	print(len(data))
 
 	# print(find_centroids(data))
-	print(data)
-	print(type(data))
-	print(euclidean_dist(data.iloc[0],data.iloc[1]))
+	# print(data)
+	# print(type(data))
+	# print(euclidean_dist(data.iloc[0],data.iloc[1]))
 	# a = data.iloc[0][0]
 	# print(a)
 	# print(a.iloc[0,1])
